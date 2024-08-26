b278a7ce5c699eb36ce5a0d7057ddc8f6f884a12
kyracho <wsc2119@columbia.edu>
2024-08-26 16:48:13 -0700

    Initial Commit

---
+++ RPN Calculator Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RPN Calculator Watch App
+//
+//  Created by Kyra Cho on 8/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RPN Calculator Watch App/RPN_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPN_CalculatorApp.swift
+//  RPN Calculator Watch App
+//
+//  Created by Kyra Cho on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct RPN_Calculator_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
